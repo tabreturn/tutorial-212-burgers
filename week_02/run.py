cd8bbd6df92d891f438c39158f767d97bb57797d
tabreturn <github@tabreturn.com>
2018-03-22 16:49:03 +1300

    - convert menu items to lists

--- week_02/run.py
+++ week_02/run.py
@@ -8,6 +8,27 @@ app = Flask(__name__)
 # export FLASK_DEBUG=1
 # flask run
 
+burgers = [
+ ['Classic Burger', '$4.99'],
+ ['Cheese Burger', '$5.99'],
+ ['Chicken Burger', '$5.99'],
+ ['Double Burger', '$6.99']
+]
+
+drinks = [
+ ['Cola', '$0.99'],
+ ['Ginger Ale', '$0.99'],
+ ['Beer', '$1.99'],
+ ['Coffee', '$1.99']
+]
+
+sides = [
+ ['Fries', '$1.99'],
+ ['Onion Rings', '$1.99'],
+ ['Mushrooms', '$1.99'],
+ ['Salad', '$1.99']
+]
+
 @app.route('/')
 def index():
-  return render_template('index.html', disclaimer='may contain traces of nuts')
+  return render_template('index.html', disclaimer='may contain traces of nuts', burgers=burgers, drinks=drinks, sides=sides)
