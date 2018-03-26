9fd23d30215acb3bcec15667f82ca18698d882cc
tabreturn <github@tabreturn.com>
2018-03-26 18:44:28 +1300

    - create week 03 content (duplicate of week 02) and a database

---
+++ week_03/run.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# to run:
+# cd to directory
+# export FLASK_APP=run.py
+# export FLASK_DEBUG=1
+# flask run
+
+burgers = [
+ ['Classic Burger', '$4.99'],
+ ['Cheese Burger', '$5.99'],
+ ['Chicken Burger', '$5.99'],
+ ['Double Burger', '$6.99']
+]
+
+drinks = [
+ ['Cola', '$0.99'],
+ ['Ginger Ale', '$0.99'],
+ ['Beer', '$1.99'],
+ ['Coffee', '$1.99']
+]
+
+sides = [
+ ['Fries', '$1.99'],
+ ['Onion Rings', '$1.99'],
+ ['Mushrooms', '$1.99'],
+ ['Salad', '$1.99']
+]
+
+@app.route('/')
+def index():
+  return render_template('index.html', disclaimer='may contain traces of nuts', burgers=burgers, drinks=drinks, sides=sides)
+
+@app.route('/order')
+def order():
+  return render_template('order.html')
