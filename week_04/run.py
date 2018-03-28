37415a2ae3fadb421cf6af3dc1658ec6bf17c588
tabreturn <github@tabreturn.com>
2018-03-28 18:16:48 +1300

    - duplicate week 03 content for week 04 and bump-up button size

---
+++ week_04/run.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, g
+import sqlite3
+
+app = Flask(__name__)
+
+# to run:
+# cd to directory
+# export FLASK_APP=run.py
+# export FLASK_DEBUG=1
+# flask run
+
+MENUDB = 'menu.db'
+
+@app.route('/')
+def index():
+  con = sqlite3.connect(MENUDB)
+
+  burgers = []
+  free = '0'
+  #cur = con.execute('SELECT burger,price FROM burgers WHERE price>=' + free)
+  cur = con.execute('SELECT burger,price FROM burgers WHERE price>=?', (free,))
+  for row in cur:
+    burgers.append(list(row))
+
+  drinks = []
+  cur = con.execute('SELECT drink,price FROM drinks')
+  for row in cur:
+    drinks.append(list(row))
+
+  sides = []
+  cur = con.execute('SELECT side,price FROM sides')
+  for row in cur:
+    sides.append(list(row))
+
+  con.close()
+
+  return render_template('index.html', disclaimer='may contain traces of nuts', burgers=burgers, drinks=drinks, sides=sides)
+
+@app.route('/order')
+def order():
+  return render_template('order.html')
