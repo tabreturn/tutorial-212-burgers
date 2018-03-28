93eec190d6d3881afd2449410345afeeea886f71
tabreturn <github@tabreturn.com>
2018-03-28 17:57:08 +1300

    - add parameterized example

--- week_03/run.py
+++ week_03/run.py
@@ -11,28 +11,26 @@ app = Flask(__name__)
 
 MENUDB = 'menu.db'
 
-drinks = [
- ['Cola', '$0.99'],
- ['Ginger Ale', '$0.99'],
- ['Beer', '$1.99'],
- ['Coffee', '$1.99']
-]
-
-sides = [
- ['Fries', '$1.99'],
- ['Onion Rings', '$1.99'],
- ['Mushrooms', '$1.99'],
- ['Salad', '$1.99']
-]
-
 @app.route('/')
 def index():
   con = sqlite3.connect(MENUDB)
-  cur = con.execute('SELECT burger,price FROM burgers')
-  burgers = []
 
+  burgers = []
+  free = '0'
+  #cur = con.execute('SELECT burger,price FROM burgers WHERE price>=' + free)
+  cur = con.execute('SELECT burger,price FROM burgers WHERE price>=?', (free,))
   for row in cur:
     burgers.append(list(row))
+
+  drinks = []
+  cur = con.execute('SELECT drink,price FROM drinks')
+  for row in cur:
+    drinks.append(list(row))
+
+  sides = []
+  cur = con.execute('SELECT side,price FROM sides')
+  for row in cur:
+    sides.append(list(row))
 
   con.close()
 
